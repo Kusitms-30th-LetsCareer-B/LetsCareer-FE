f49d586c08c3583de0ae3ad4bdfa044696f6421b
Hee Sang <118061713+codrin2@users.noreply.github.com>
2024-09-05 20:53:50 +0900

    eslint 검사 조건 완화

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,8 @@ export default tseslint.config(
         "warn",
         { allowConstantExport: true },
       ],
+      "no-unused-vars": "warn", // 사용되지 않는 변수를 경고로만 처리
+      "@typescript-eslint/no-unused-vars": "warn", // TypeScript에서 사용되지 않는 변수를 경고로 처리
     },
   },
 );
