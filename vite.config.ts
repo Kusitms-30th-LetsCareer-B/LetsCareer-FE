392d648a243ef2529365bf2cd181f8ac0c379b6f
kimzini <rlawlsdl11111@gmail.com>
2024-09-06 15:45:35 +0900

    [fix] 에러 무시 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    logOverride: { 'ts(6133)': 'silent' } // TS6133 에러 무시 (사용되지 않는 변수)
+  }
 });
