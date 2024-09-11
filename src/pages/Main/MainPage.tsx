b26f4e5e93811b6b6e79bf90c1ea824e68de95df
inaemon <github32377@gmail.com>
2024-09-12 00:23:23 +0900

    [design] 메인홈 알림창 디자인 수정

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -6,8 +6,8 @@ import MainCalendar from "../../components/Calendar";
 import PopUpAlarmButton from "./components/PopUpAlarm.tsx"
 
 // 투두리스트
-import CompanyTodoListFixedComponent from "../Calendar/components/CalendarTodoListFixed.tsx"
-import CompanyTodoListFixedComponent_20240909_날짜별 from "../Calendar/components/CalendarTodoListFixed_20240909_날짜별.tsx"
+import TodoListFixedComponent from "../Calendar/components/TodoListFixed.tsx"
+import TodoListFixedComponent_20240909_날짜별 from "../Calendar/components/TodoListFixed_20240909_날짜별.tsx"
 
 // Date 관련 hook 임포트
 import { getYearMonthDay, getYear, getMonth, getFormattedDate3 } from "../../shared/hooks/useDate.ts";
@@ -35,7 +35,7 @@ function MainPage({userId, userName} : userInfo) {
 
   return (
     <div className="p-[48px]">
-    {/** 타이틀 */}
+    {/** 타이틀 <TodoComponent/>*/}
     <div className="flex justify-between">
       <div className="font-bold text-medium24 text-neutral-0 mb-10">
         {userName}님, 오늘도 커리어를 향해 함께 같이 달려봐요!
@@ -71,11 +71,10 @@ function MainPage({userId, userName} : userInfo) {
           
           {/** 투두 리스트 */}
           <div className="font-sans rounded-lg border border-neutral-80 px-5">
-            <CompanyTodoListFixedComponent  userId={userId} />
+            <TodoListFixedComponent  userId={userId} />
             {/** 
-            <CompanyTodoListFixedComponent_20240909_날짜별  userId={userId} selectedDate={selectedDate} setSelectedDate={setSelectedDate}  selectedDateString ={getFormattedDate3(selectedDate)}  />
+            <TodoListFixedComponent_20240909_날짜별  userId={userId} selectedDate={selectedDate} setSelectedDate={setSelectedDate}  selectedDateString ={getFormattedDate3(selectedDate)}  />
             */}
-            <CompanyTodoListFixedComponent_20240909_날짜별  userId={userId} selectedDate={selectedDate} setSelectedDate={setSelectedDate}  selectedDateString ={getFormattedDate3(selectedDate)}  />
           </div>
         </div>
       </div>
