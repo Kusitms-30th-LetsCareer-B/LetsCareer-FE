a06da99a741519bc9233568bc5c6372b1d68a5cf
kimzini <rlawlsdl11111@gmail.com>
2024-09-12 17:22:11 +0900

    [design] 채용 일정 추가 페이지 디자인 수정

--- src/pages/RecruScheduleEnv/RecruScheduleEnvPage.tsx
+++ src/pages/RecruScheduleEnv/RecruScheduleEnvPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useGoBack } from "../../shared/hooks/useGoBack"; // 이전 페이지로 전환하기 위한 훅
 
 // API 연동 모듈 임포트
@@ -15,9 +15,14 @@ import favoriteStarIconFilled from "../../shared/assets/favoriteStarFilled.png";
 
 // Date 관련 훅
 import { getFormattedDate3 } from "../../shared/hooks/useDate"
+import { GoBackButton } from '../../components/Buttons/Button';
 
 // 컴포넌트
 const ScheduleEnvPage = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   const [companyName, setCompanyName] = useState('');
   const [task, setTask] = useState('');
   const [announcementUrl, setAnnouncementUrl] = useState('');
@@ -74,16 +79,12 @@ const ScheduleEnvPage = () => {
 
 
   return (
-    <div className="bg-static-100 rounded-sm p-10">
-      {/** 타이틀 */}
-      <div className="flex justify-start items-center text-center font-bold text-medium24 text-neutral-0">
-        <div className="flex items-center">
-          <img src={prevButtonIcon} className="h-[16px] cursor-pointer" onClick={goBack}/>
-        </div>
-        <div className="flex items-center px-5">
-          관심 기업의 채용 일정을 등록하고 관리하세요!
-        </div>
+    <div className="bg-static-100 h-[780px] rounded-sm p-10">
+      <div className='flex justify-between items-center'>
+        {/** 타이틀 */}
+        <GoBackButton text="관심 기업의 채용 일정을 등록하고 관리하세요!" />
       </div>
+      
 
       {/** 기업 정보 입력 */}
       <div className="grid grid-cols-2 py-2 gap-4" style={{ gridTemplateColumns: "100px minmax(0px, 1fr)" }}>
@@ -99,7 +100,7 @@ const ScheduleEnvPage = () => {
         <div className="flex justify-start items-center">
           <input
             type="text"
-            className="text-start rounded-xs border border-neutral-80 font-regular text-xsmall16 text-neutral-45 p-5 min-w-[550px]"
+            className="text-start rounded-sm border border-neutral-80 font-regular text-xsmall16 text-neutral-30 p-5 min-w-[550px] px-[20px] py-[14px] placeholder:text-neutral-45 focus:outline-none"
             placeholder="기업명 검색"
             value={companyName}
             onChange={(e) => setCompanyName(e.target.value)}
@@ -130,7 +131,7 @@ const ScheduleEnvPage = () => {
         <div className="flex justify-start items-center">
           <input
             type="text"
-            className="text-start rounded-xs border border-neutral-80 font-regular text-xsmall16 text-neutral-45 p-5 min-w-[550px]"
+            className="text-start rounded-sm border border-neutral-80 font-regular text-xsmall16 text-neutral-30 min-w-[550px] px-[20px] py-[14px] placeholder:text-neutral-45 focus:outline-none"
             placeholder="직무 입력"
             value={task}
             onChange={(e) => setTask(e.target.value)}
@@ -143,7 +144,7 @@ const ScheduleEnvPage = () => {
           <div className="text-start font-semibold text-small18 text-system-error">*</div>
         </div>
         <div className="flex justify-start items-center">
-          <div className="flex justify-between text-start rounded-xs border border-neutral-80 font-regular text-xsmall16 text-neutral-45 p-5 min-w-[248px]" onClick={handleOpenStartDatePicker}>
+          <div className={`flex justify-between text-start rounded-sm border border-neutral-80 font-regular text-xsmall16 min-w-[248px] px-[20px] py-[14px] ${selectedStartDate ? "text-neutral-30" : "text-neutral-45"}`} onClick={handleOpenStartDatePicker}>
             {/* 선택된 날짜 포매팅 후 출력 */}
             {selectedStartDate ? getFormattedDate3(selectedStartDate) : '시작일'}
             
@@ -151,7 +152,7 @@ const ScheduleEnvPage = () => {
             <PopUpDatePicker onDateSelected={handleStartDateSelected}/>
           </div>
           <div className="px-5">~</div>
-          <div className="flex justify-between text-start rounded-xs border border-neutral-80 font-regular text-xsmall16 text-neutral-45 p-5 min-w-[248px]" onClick={handleOpenEndDatePicker}>
+          <div className={`flex justify-between text-start rounded-sm border border-neutral-80 font-regular text-xsmall16 min-w-[248px] px-[20px] py-[14px] ${selectedEndDate ? "text-neutral-30" : "text-neutral-45"}`} onClick={handleOpenEndDatePicker}>
             {/* 선택된 날짜 포매팅 후 출력 */}
             {selectedEndDate ? getFormattedDate3(selectedEndDate) : '마감일'}
             
@@ -166,7 +167,7 @@ const ScheduleEnvPage = () => {
           <div className="text-start font-regular text-xsmall16 text-neutral-45 mx-2 min-w-[250px]">
             마감일 리마인드 알림을 드릴까요?
           </div>
-          <div className="mx-2">
+          <div className="">
             <input type="checkbox" checked={isRemind} onChange={() => setIsRemind(!isRemind)} />
           </div>
         </div>
@@ -178,18 +179,18 @@ const ScheduleEnvPage = () => {
         <div className="flex justify-start items-center">
           <input
             type="text"
-            className="text-start rounded-xs border border-neutral-80 font-regular text-xsmall16 text-neutral-45 p-5 min-w-[550px]"
+            className="text-start rounded-sm border border-neutral-80 font-regular text-xsmall16 text-neutral-30 min-w-[550px] px-[20px] py-[14px] placeholder:text-neutral-45 focus:outline-none"
             placeholder="공고 링크"
             value={announcementUrl}
             onChange={(e) => setAnnouncementUrl(e.target.value)}
           />
         </div>
       </div>
-
-      {/** 등록 버튼 */}
-      <button className="mt-5 px-10 py-3 bg-primary-100 text-white rounded-sm" onClick={handleSubmit}>
-        등록하기
-      </button>
+      <div className='flex justify-end'>
+        <button className="px-[20px] py-[12px] min-w-[300px] mt-[40px] mb-[40px] mr-[130px] bg-primary-100 text-white rounded-md" onClick={handleSubmit}>
+            등록하기
+        </button>
+      </div>
     </div>
   );
 };
